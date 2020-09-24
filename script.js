0416764371b5b57180b050926157c3fc4bc865ad
orhiley90 <69226157+orhiley90@users.noreply.github.com>
2020-09-23 20:06:54 -0700

    button makes new buttons they dont have text atm

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
-
-let latitude = 32.2226;
+let destination = ""; 
+let latitude = -32.2226;
 let longitude = 110.9747;
 let weatherKey = "6566b7af6a83b978b652888cbc883c0a";
 let weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon" + longitude + "&appid=" + weatherKey;
 
 // calls 
 function checkWeather() {
-
-    let latitude = 0;
-    let longitude = 0;
     let weatherKey = "6566b7af6a83b978b652888cbc883c0a";
-    // let weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon" + longitude + "&appid=" + weatherKey;
     let weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,daily&appid=" + weatherKey;
     $.ajax({
         url: weatherURL,
         method: "GET"
     }).then(function (response) {
-        
         console.log(response);
     });
 }
-
+$("#inputButton").on("click", function (e) {
+    e.preventDefault();
+    
+        let button = $('<input type="button">');
+        $(button).attr("value", destination);
+        $("#searchedButton").append(button);
+    })
 checkWeather();
-
 })
